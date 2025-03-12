eecbe52437ca33e11282b9ecd3f15c8a33e13387
vierui <ruivieira.pro@gmail.com>
2025-03-13 00:53:57 +0100

    Remove temporary learning files

--- scripts/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from .time_series import build_gen_time_series, build_demand_time_series
-from .helpers import ask_user_confirmation
-
-__all__ = [
-    'build_gen_time_series',
-    'build_demand_time_series',
-    'ask_user_confirmation'
-] 
